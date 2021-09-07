2383e45e4b40a43885950846b26cdbe709919345
Cooler_Noctua <jonginchoi99@gmail.com>
2021-09-07 21:52:47 +0900

    20210907

---
+++ src/bronze/Bronze5_1001.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Bronze5_1001 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println(sc.nextInt()-sc.nextInt());
+		
+		sc.close();
+	}	
+}
---
+++ src/silver/Silver5_1010.java
@@ -0,0 +1,20 @@
+package silver;
+
+import java.util.Scanner;
+
+public class Silver5_1010 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		
+		int testCount;
+		int N,M;
+		testCount=sc.nextInt();
+		
+		for(int i=0;i<testCount;i++) {
+			N=sc.nextInt();
+			M=sc.nextInt();
+		}
+		
+		sc.close();
+	}
+}
