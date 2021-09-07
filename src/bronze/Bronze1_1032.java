4a83ea15b93e721c9cc587aaaf732e613f386303
Cooler_Noctua <jonginchoi99@gmail.com>
2021-09-07 09:52:33 +0900

    20210907

---
+++ src/bronze/Bronze1_1032.java
@@ -0,0 +1,31 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Bronze1_1032 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		String[] str = new String[count];
+		str[0] = sc.next();
+		int length = str[0].length();
+		char[] arr1, arr2 = new char[length];
+		arr1 = str[0].toCharArray();
+		for (int i = 1; i < count; i++) {
+			str[i] = sc.next();
+			arr2 = str[i].toCharArray();
+			for (int j = 0; j < length; j++) {
+				if ((arr1[j] == '?') || (arr1[j] == arr2[j])) {
+					continue;
+				} else {
+					arr1[j] = '?';
+				}
+			}
+
+		}
+		String strfinal = new String(arr1);
+		System.out.println(strfinal);
+
+		sc.close();
+	}
+}
