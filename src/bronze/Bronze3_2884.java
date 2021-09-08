5375a7c30a2295a6f7dcf9beffa5d5121e66b1db
Cooler_Noctua <jonginchoi99@gmail.com>
2021-09-08 22:13:36 +0900

    20210903

---
+++ src/bronze/Bronze3_2884.java
@@ -0,0 +1,26 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Bronze3_2884 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int hour, minute;
+		hour=sc.nextInt();
+		minute=sc.nextInt();
+		
+		if(minute>=45) {
+			minute=minute-45;
+		}else {
+			if(hour==0) {
+				hour=24;
+			}
+			hour=hour-1;
+			minute=minute+15;
+		}
+		System.out.println(hour+" "+minute);
+	}
+
+}
