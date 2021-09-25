5fb91d5d8f233e3217441f3ebdc9ac9629727948
Noctua99 <jonginchoi99@gmail.com>
2021-09-25 09:44:03 +0900

    20210925

--- src/silver/Silver5_1934.py
+++ src/silver/Silver5_1934.py
@@ -1,13 +1,29 @@
 import sys
 import math
 
+# 반복문을 이용
+def gcd(a, b):
+    while b > 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
+# 재귀함수를 이용
+def gcdRecur(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcdRecur(b, a % b)
+
+
 T = int(sys.stdin.readline())
 
 s = ""
 for i in range(T):
     A, B = map(int, sys.stdin.readline().split())
-    gcd = math.gcd(A, B)
-    lcm = int(A*B/gcd)
+    gcdAB = gcdRecur(A, B)
+    lcm = int(A*B/gcdAB)
     # sys.stdout.write(str(lcm)+'\n')
     s = s+str(lcm)+'\n'
     # print(lcm)
