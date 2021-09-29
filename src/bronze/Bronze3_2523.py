e7bfa9528558c8bf9d2bc672f4b77ad8b267d9e1
Noctua99 <jonginchoi99@gmail.com>
2021-09-29 10:58:07 +0900

    20210929

--- src/bronze/Bronze3_2523.py
+++ src/bronze/Bronze3_2523.py
@@ -2,9 +2,10 @@ N = int(input())
 
 
 result = ""
+for i in range(1, N+1):
+    result += '*'*i+'\n'
 
-for i in range(1,N+1):
-    result+='*'*i+'\n'
-for i in range(N-1,0,-1):
-    result+='*'*i+'\n'
-print(result,end='')
+for i in range(N-1, 0, -1):
+    result += '*'*i+'\n'
+
+print(result, end='')
