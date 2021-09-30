dadb0a8aeaa9f19923f6dbed61221a10249296f2
Noctua99 <jonginchoi99@gmail.com>
2021-09-30 09:08:59 +0900

    20210930

---
+++ src/silver/Silver2_1912.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+lst = list(map(int, sys.stdin.readline().split()))
+dp = [0]*N
+dp[0] = lst[0]
+maximum = lst[0]
+
+for i in range(1, N):
+    dp[i] = max(dp[i-1]+lst[i], lst[i])
+    maximum = max(dp[i], maximum)
+    print(f'dp[{i} : {dp[i]} maximum : {maximum}]')
+
+sys.stdout.write(str(maximum))
