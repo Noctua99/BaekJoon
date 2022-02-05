c2cd7a5e4ae298eddbfb3bf3965453fff06366ba
Noctua99 <jonginchoi99@gmail.com>
2022-02-05 21:53:13 +0900

    2022_02_05

--- src/Silver/Silver5_11728.java
+++ src/Silver/Silver5_11728.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Silver4_2870 {
+public class Silver5_11728 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
