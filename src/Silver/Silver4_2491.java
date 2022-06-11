f4f99c6096a47cd069c2bb9436645adf72455252
jongi <jonginchoi99@gmail.com>
2022-06-11 21:46:11 +0900

    Dynamic Programming

---
+++ src/Silver/Silver4_2491.java
@@ -0,0 +1,51 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Silver4_2491 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxLen = 1;
+        int tempLen = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i] >= arr[i - 1]) {
+                tempLen++;
+            } else {
+                maxLen = Math.max(maxLen, tempLen);
+                tempLen = 1;
+            }
+        }
+
+        maxLen = Math.max(maxLen, tempLen);
+        tempLen = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i] <= arr[i - 1]) {
+                tempLen++;
+            } else {
+                maxLen = Math.max(maxLen, tempLen);
+                tempLen = 1;
+            }
+        }
+
+        maxLen = Math.max(maxLen, tempLen);
+
+        System.out.println(maxLen);
+    }
+}
