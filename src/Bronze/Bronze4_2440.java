0da431404e4f2567621310d91caef09c74744f89
jongi <jonginchoi99@gmail.com>
2022-07-23 00:20:35 +0900

    Implementation

---
+++ src/Bronze/Bronze4_2440.java
@@ -0,0 +1,22 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze4_2440 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 1; i--) {
+            sb.append("*".repeat(i)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
